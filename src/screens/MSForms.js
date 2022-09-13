d4a79e99a3ca123ba2e369065483b0d34f75fd3e
Anuruddh Singh <109131341+69Moto@users.noreply.github.com>
2022-09-13 16:32:06 +0530

    initial

---
+++ src/screens/MSForms.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import Fifth from './Fifth';
+import First from './First';
+import Fourth from './Fourth';
+import Second from './Second';
+import Third from './Third';
+import './Style.css';
+
+
+export default function MSForms() {
+
+    const [page, setPage] = useState(0);
+
+    const [formData, setFormData] = useState({
+        AccountTransactionID: '',
+        TransactionID: '',
+        TransactionAmount: '',
+        //TransactionTimeStamp : '',
+        LinkedAccountID: '',
+
+        MCCCodeID: '',
+        ActiveFlag: '', //Yes or No
+        TransactionCurrencyCodeID: '',
+        TransactionModeID: '', //Yes or No
+        CategoryID: '',
+
+        MerchantID: '',
+        TransactionType: '', //Credit or Debit
+        NotAExpense: '',
+        BalanceAmount: '',
+        DestinationCurrency: '',
+
+        CurrencyCodeID: '',
+        AccountNumber: '',
+        BankID: '',
+        Narration: '',
+        TransactionReferenceNumber: '',
+
+        CategorizedFlag: false, // Yes or No
+
+    })
+
+    const MSScreens = [
+
+        <First
+            page={page}
+            setPage={setPage}
+            formData={formData}
+            setFormData={setFormData}
+        />,
+
+        <Second
+            page={page}
+            setPage={setPage}
+            formData={formData}
+            setFormData={setFormData}
+        />,
+
+        <Third
+            page={page}
+            setPage={setPage}
+            formData={formData}
+            setFormData={setFormData}
+        />,
+
+        <Fourth
+            page={page}
+            setPage={setPage}
+            formData={formData}
+            setFormData={setFormData}
+        />,
+
+        <Fifth
+            page={page}
+            setPage={setPage}
+            formData={formData}
+            setFormData={setFormData}
+        />,
+
+    ];
+
+    return (
+        <>
+            <div className='form-step'>
+                Page {page + 1}
+            </div>
+            <div className='form-box'>
+                {MSScreens[page]}
+            </div>
+        </>
+    )
+}
