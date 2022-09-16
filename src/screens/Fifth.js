b9ce47518652d5cef8c63649780e292ca466a595
shivam <shivam@clayfin.com>
2022-09-16 16:40:46 +0530

    ye

--- src/screens/Fifth.js
+++ src/screens/Fifth.js
@@ -6,38 +6,43 @@ import Img1 from "../components/Images/Img1.jpeg"
 
 export default function Fifth({ page, setPage, formData, setFormData }) {
 
-    const handleSubmit = () => {
+    const handleSubmit = async() => {
         var userData = {
-            AccountTransactionID: formData.AccountTransactionID, 
-            TransactionID: formData.TransactionID, 
-            TransactionAmount: formData.TransactionAmount, 
+            AccountTransactionID: formData.AccountTransactionID,
+            TransactionID: formData.TransactionID,
+            TransactionAmount: formData.TransactionAmount,
             LinkedAccountID: formData.LinkedAccountID,
-            MCCCodeID: formData.MCCCodeID, 
-            ActiveFlag: formData.ActiveFlag, 
-            TransactionCurrencyCodeID: formData.TransactionCurrencyCodeID, 
-            TransactionModeID: formData.TransactionModeID, 
+            MCCCodeID: formData.MCCCodeID,
+            ActiveFlag: formData.ActiveFlag,
+            TransactionCurrencyCodeID: formData.TransactionCurrencyCodeID,
+            TransactionModeID: formData.TransactionModeID,
             CategoryID: formData.CategoryID,
-            MerchantID: formData.MerchantID, 
-            TransactionType: formData.TransactionType, 
-            NotAExpense: formData.NotAExpense, 
-            BalanceAmount: formData.BalanceAmount, 
+            MerchantID: formData.MerchantID,
+            TransactionType: formData.TransactionType,
+            NotAExpense: formData.NotAExpense,
+            BalanceAmount: formData.BalanceAmount,
             DestinationCurrency: formData.DestinationCurrency,
-            CurrencyCodeID: formData.CurrencyCodeID, 
-            AccountNumber: formData.AccountNumber, 
-            BankID: formData.BankID, 
-            Narration: formData.Narration, 
+            CurrencyCodeID: formData.CurrencyCodeID,
+            AccountNumber: formData.AccountNumber,
+            BankID: formData.BankID,
+            Narration: formData.Narration,
             TransactionReferenceNumber: formData.TransactionReferenceNumber,
             CategorizedFlag: formData.CategorizedFlag,
         };
-        axios.post("https://pfmfake.herokuapp.com/transcation", userData).then((response) => {
-            console.log(response.status);
-            console.log("asasas",userData)
-        });
-        // fetch('https://pfmfake.herokuapp.com/transcation', {
-        //     method: 'POST',
-        //     headers: {'Content-Type':'application/x-www-form-urlencoded'}, // this line is important, if this content-type is not set it wont work
-        //     body: userData
+        // axios.post("https://pfmfake.herokuapp.com/transcation", userData).then((response) => {
+        //     console.log(response.status);
+        //     console.log("asasas",userData)
         // });
+
+        var data = new FormData();
+        data.append("json", JSON.stringify(userData));
+        const out = await fetch('https://pfmfake.herokuapp.com/transcation',
+        {
+            method: 'POST',
+            body: data,
+        });
+        console.log(out)
+        
     };
     return (
         <>
